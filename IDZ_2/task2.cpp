f881ff76d33416d551e12212c2723334683f20f2
fort1go <104702432+fort1go@users.noreply.github.com>
2025-11-17 11:16:11 +1000

    Update task2.cpp

--- IDZ_2/task2.cpp
+++ IDZ_2/task2.cpp
@@ -1,1 +1,22 @@
-//Hello world
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a, a2, a4, a5, a8, a13;
+    cout << "Введите a: ";
+    cin >> a;
+
+    a2 = a * a;
+    a4 = a2 * a2;
+    a5 = a4 * a;
+    a8 = a4 * a4;
+    a13 = a8 * a5;
+
+    cout << "Входные данные - " << a << endl;
+    cout << "a5 - " << a5 << endl;
+    cout << "a13 - " << a13 << endl;
+
+    return 0;
+}
