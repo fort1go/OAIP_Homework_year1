48612c04f91fc83067994d2d473bab6b46c03db5
fort1go <104702432+fort1go@users.noreply.github.com>
2025-11-17 10:55:33 +1000

    Update task1.cpp

--- IDZ_2/task1.cpp
+++ IDZ_2/task1.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
+int main() {
+    double x, y;
+    cout << "Введите x и y: ";
+    cin >> x >> y;
+    cout << (fabs(x) - fabs(y)) / (1 + fabs(x * y)) << endl;
+    return 0;
+}
